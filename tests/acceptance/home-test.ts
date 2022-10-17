21c0af384796a53dd36116afad5cd239ff671a48
Brian Finck <brian@brianfinck.com>
2022-10-16 18:51:28 -0700

    Added mocked in test coverage

---
+++ tests/acceptance/home-test.ts
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | application role', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('the download list contains 5 downloads', async function (assert) {});
+
+  test('a user can select a download and gets a alert with that download', async function (assert) {});
+
+  test('a user gets a help message when selecting download without a download selected', async function (assert) {});
+
+  test('a user gets a help message when selecting download without a download selected', async function (assert) {});
+
+  test('a user can deselect a download', async function (assert) {});
+
+  test('a user can select all downloads with the select all button', async function (assert) {});
+});
